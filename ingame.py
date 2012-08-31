b99acfcf73df1158ddc8b98146b6cc2cad38a81f
styts <github@styts.com>
2012-08-31 22:54:31 +0200

    display a simle sprite

--- ingame.py
+++ ingame.py
@@ -1,9 +1,14 @@
 from dvizshok.appstate import AppState
+import pygame
 
 
 class InGame(AppState):
     def process_input(self, event):
-        pass
+        if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+            self.next_state = ("GoodBye", None)
 
     def draw(self):
+        tree = self.app.resman.get_surface("tree")
+        self.app.screen.blit(tree, (10, 20))
+        self.app.dirty(pygame.Rect(10, 20, 10, 20))
         pass
