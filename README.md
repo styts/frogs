7fe5678e93f8e88395b155520630edbf4aa7129f
styts <github@styts.com>
2012-09-01 13:51:39 +0200

    foo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Frogs
 =====
 Sample game that uses "dvizshok" pygame engine and runs on desktops and android devices alike.
+
+Notes
+-----
+It's not even a WIP, it's just a platform for various features to test my game engine. 
+I am not planning to make a game out of this.
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from ingame import InGame
 
 
 class MyApp(App):
-    pass
+    def init(self):
+        self.resman.load_font("default", 20)
+        self.font = self.resman.get_font("default_20")
 
 #res = (480, 320)
-res = (800, 600)
+res = (800, 480)
+#res = (800, 600)
 #res = (1024, 768)
-app = MyApp("Frogs", resolution=res, appstates=[InGame])
+app = MyApp("Frogs", resolution=res, appstates=[InGame], fps=100)
 app.run()
